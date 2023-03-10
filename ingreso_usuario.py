370e813d91ed3e4122dd734ca112bc8b51db2796
erck2018 <erick.florez@correounivalle.edu.co>
2023-03-10 09:48:03 -0500

    token

---
+++ ingreso_usuario.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+url = "http://zafiroapi.sispropreprod.gov.co:8081/api/Login"
+
+payload = json.dumps({
+  "tipoIdentificacion": "CC",
+  "Identificacion": "779713047",
+  "Password": "Pruebas123"
+})
+headers = {
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+response_dict = response.json()
+
+token = response_dict.get('token')
+#print(response.text)
+print(token)
+
+#variable="token"
+#authen=f"beard {token}"
